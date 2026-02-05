8f862a83620f14a412ff8784518ca86e665e20d7
acceberH <qh2130@nyu.edu>
2026-02-05 02:06:44 -0800

    set depsLockFilePath for amplify function

--- amplify/functions/beaver-api/resource.js
+++ amplify/functions/beaver-api/resource.js
@@ -1,7 +1,9 @@
 const { defineFunction } = require("@aws-amplify/backend");
+const path = require("node:path");
 
 exports.beaverApi = defineFunction({
   name: "beaver-api",
   runtime: 18,
   entry: "./index.js",
+  depsLockFilePath: path.join(__dirname, "package-lock.json"),
 });
