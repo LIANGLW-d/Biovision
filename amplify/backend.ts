afc06cc81c4475c4a0dd9c2b9b098b2fa9656807
acceberH <qh2130@nyu.edu>
2026-02-05 01:46:04 -0800

    ignore amplify resource type check

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend } from "@aws-amplify/backend";
+// @ts-ignore - Amplify generates JS function resources without TS typings.
 import { beaverApi } from "./functions/beaver-api/resource";
 
 defineBackend({
