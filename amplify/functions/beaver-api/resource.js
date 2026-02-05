01ac3c8692d8f4d6a5c47606c4dd138599ea2540
acceberH <qh2130@nyu.edu>
2026-02-05 02:09:52 -0800

    use relative depsLockFilePath for amplify function

--- amplify/functions/beaver-api/resource.js
+++ amplify/functions/beaver-api/resource.js
@@ -5,9 +5,5 @@ exports.beaverApi = defineFunction({
   name: "beaver-api",
   runtime: 18,
   entry: "./index.js",
-  depsLockFilePath: path.resolve(
-    __dirname,
-    "../../..",
-    "amplify/functions/beaver-api/package-lock.json",
-  ),
+  depsLockFilePath: "amplify/functions/beaver-api/package-lock.json",
 });
