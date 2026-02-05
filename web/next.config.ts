ae7d30842db922c9afc004291c7c8d30e24273a6
acceberH <qh2130@nyu.edu>
2026-02-05 00:14:03 -0800

    use serverExternalPackages for aws sdk

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,24 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    serverComponentsExternalPackages: [
-      "@aws-sdk/client-s3",
-      "@aws-sdk/client-sqs",
-      "@aws-sdk/client-bedrock-runtime",
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push(
-        "@aws-sdk/client-s3",
-        "@aws-sdk/client-sqs",
-        "@aws-sdk/client-bedrock-runtime",
-      );
-    }
-    return config;
-  },
+  serverExternalPackages: [
+    "@aws-sdk/client-s3",
+    "@aws-sdk/client-sqs",
+    "@aws-sdk/client-bedrock-runtime",
+  ],
 };
 
 export default nextConfig;
